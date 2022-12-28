c8595ba7bda3041116826c991e9c5afe01246da1
MaybeAN <mailnitti@gmail.com>
2022-12-28 01:05:42 +0100

    Integrazione

--- demo/controllers/tea.js
+++ demo/controllers/tea.js
@@ -1,3 +1,5 @@
+const Tea = require('../models/tea');
+
 console.log("controller");
 const newTea = (req, res, next) => {
     console.log("eccomi");
--- demo/server.js
+++ demo/server.js
@@ -2,12 +2,21 @@ const dotenv = require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
 const routes = require('./routes/tea');
 
 app.use(express.json());
 app.use('/', routes);
 
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {usedNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if(err) return console.log("Error: ", err);
+        console.log("MongoDB conn -- state: ", mongoose.connection.readyState);
+    }
+);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('Your app is listening on port ' + listener.address().port)
-})
+});
