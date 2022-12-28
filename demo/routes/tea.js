aaa46690dff612445d661c1343bb4ee421986b57
MaybeAN <mailnitti@gmail.com>
2022-12-28 08:46:01 +0100

    Ultimo commit della mattinata

--- demo/routes/tea.js
+++ demo/routes/tea.js
@@ -1,12 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
 const teaController = require('../controllers/tea');
 
 router.post('/tea', teaController.newTea);
 /*router.get('/tea', teaController.getAllTea);
 router.delete('/tea', teaController.deleteAllTea);
-
 router.post('/tea:name', teaController.newComment);
 router.get('/tea:name', teaController.getOneTea);
 router.delete('/tea:name', teaController.deleteOneTea);*/
