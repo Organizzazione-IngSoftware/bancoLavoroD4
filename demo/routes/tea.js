ffdd184df47485f6196e766432d0eab46de19ea1
MaybeAN <mailnitti@gmail.com>
2023-01-15 16:58:42 +0100

    Ultima versione funzionante

--- demo/routes/tea.js
+++ demo/routes/tea.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const teaController = require('../controllers/tea');
 
 router.post('/tea', teaController.newTea);
-/*router.get('/tea', teaController.getAllTea);
+router.get('/tea', teaController.getAllTea);
 router.delete('/tea', teaController.deleteAllTea);
-router.post('/tea:name', teaController.newComment);
-router.get('/tea:name', teaController.getOneTea);
-router.delete('/tea:name', teaController.deleteOneTea);*/
+router.get('/tea/:name', teaController.getOneTea);
+router.delete('/tea/:name', teaController.deleteOneTea);
 
 module.exports = router;
