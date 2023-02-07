251139eb9b6e7efa1d5e72a3eadedbec92855ee2
MaybeAN <mailnitti@gmail.com>
2023-02-07 15:28:00 +0100

    Authentication tokens are working in this version

--- MoViewerNet/app/app.js
+++ MoViewerNet/app/app.js
@@ -1,36 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
-const cors = require('cors');
 
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(cors());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
-
-const tokenChecker = function(req, res, next) {
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-    if(!token) res.status(401).json( {success: false, message: 'No token provided' });
-    jwt.verify(token, process.env.SUPER_SECRET, function(err, decoded) {
-        if(err) res.status(403).json( {success: false, message: 'Token not valid'} );
-        else {
-            req.loggedUser = decoded;
-            next();
-        }
-    })
-}
-
+const authentication = require('./authentication.js');
+const tokenChecker = require('./tokenChecker.js');
 
 
 const movie = require('./movie.js');
 const serie = require('./serie.js');
 const user = require('./user.js');
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.use('/', express.static(process.env.FRONTED || 'static'));
@@ -44,6 +31,8 @@ app.use((req, res, next) => {
 });
 
 
+
+app.use('/api/v1/authentications', authentication);
 
 app.use('/api/v1/movie/create', tokenChecker);
 app.use('/api/v1/movie/deleteAll', tokenChecker);
--- MoViewerNet/static/script.js
+++ MoViewerNet/static/script.js
@@ -1,32 +1,57 @@
-var loggedUser = {}
+/**
+ * This variable stores the logged in user
+ */
+var loggedUser = {};
 
 
+function putMovie()
+{
+    var titolo = document.getElementById("movieTitolo").value;
+    var regista = document.getElementById("movieRegista").value;
+    fetch('../api/v1/movie/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': loggedUser.token
+        },
+        body: JSON.stringify( { user: loggedUser.self, titolo: titolo, regista: regista } ),
+    })
+    .then((resp) => {
+        console.log(resp);
+        return;
+    })
+    .catch( error => console.error(error) ); // If there is any error you will catch them here
 
-function login() {
+};
+/**
+ * This function is called when login button is pressed.
+ * Note that this does not perform an actual authentication of the user.
+ * A student is loaded given the specified email,
+ * if it exists, the studentId is used in future calls.
+ */
+function login()
+{
+    //get the form object
     var email = document.getElementById("loginEmail").value;
     var password = document.getElementById("loginPassword").value;
-    fetch('../api/v1/user/authentication', {
+    // console.log(email);
+
+    fetch('../api/v1/authentications', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( {keyword: email, password: password} ),
-    }).then((resp) => resp.json())
-
-    .then(function(data) {
+        body: JSON.stringify( { email: email, password: password } ),
+    })
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) { // Here you get the data to modify as you please
+        //console.log(data);
         loggedUser.token = data.token;
         loggedUser.email = data.email;
         loggedUser.id = data.id;
         loggedUser.self = data.self;
-        document.getElementById("loggedUser").innerHTML = loggedUser.email;
-        loadLendings();
+        // loggedUser.id = loggedUser.self.substring(loggedUser.self.lastIndexOf('/') + 1);
+        document.getElementById("loggedUser").textContent = loggedUser.email;
         return;
-    }).catch( error => console.error(error));
+    })
+    .catch( error => console.error(error) ); // If there is any error you will catch them here
+
 };
-
-
-
-function loadMovies() {}
-loadMovies();
-
-
-
-function insertMovie() {}
